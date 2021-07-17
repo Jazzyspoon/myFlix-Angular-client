ece3b1ac25f4d16b9ff7ff21dcfb4c71f105f39a
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-07-17 11:01:07 -0600

    updating components

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
